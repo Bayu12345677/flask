c199afad9cf7bf55eabc325de5b523f6e8789e64
Speedrun <86620608+Bayu12345677@users.noreply.github.com>
2024-03-12 18:55:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from flask import Flask, jsonify
-import os
+from cmath import pi
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask_cors import CORS
+import requests
+import shutil, os, easyocr
 
 app = Flask(__name__)
-
-
+  
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')
 
+@app.route('/ocr_bypasses', methods=['POST'])
+def ocr_bypasses():
+  if request.method == 'POST':
+    data = request.get_json(force=True)
+    url = data['url']
+    """
+    img = requests.get(url, stream=True).raw
+    img_name = 'img.jpeg'
+    with open(img_name, 'wb') as out_file:
+      shutil.copyfileobj(img, out_file)
+    """
+    os.system(f"curl -sL {url} -o img.jpeg")
+    atext = easyocr.Reader(['en'])
+    text = atext.readtext('img.jpeg')
+    return jsonify({'url':text,'result':text})
+    # return jsonify({'result':url})
+  else:
+    return 'Error'
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
